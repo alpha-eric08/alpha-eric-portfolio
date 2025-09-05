10bc6075d7384667dd01c28aff7597686e631edf
alpha-eric08 <owusuansaheric08@gmail.com>
2025-09-05 10:52:44 +0000

    added details

--- src/components/contact/ContactLeft.jsx
+++ src/components/contact/ContactLeft.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { FaFacebookF, FaTwitter, FaLinkedinIn } from "react-icons/fa";
+import { FaFacebookF, FaTwitter, FaLinkedinIn, FaGithub } from "react-icons/fa";
 import { contactImg } from "../../assets/image/index";
 
 const ContactLeft = () => {
@@ -11,37 +11,38 @@ const ContactLeft = () => {
         alt="contactImg"
       />
       <div className="flex flex-col gap-4">
-        <h3 className="text-3xl font-bold text-white">John Doe</h3>
+        <h3 className="text-3xl font-bold text-white">Owusu-Ansah Eric</h3>
         <p className="text-lg font-normal text-gray-400">
-          MERN Stack Developer
+         Frontend Developer || React.js, Tailwind CSS & Firebase || Javascript || Open to Mentorship & Project Collaborations|| IOT Enthusiast || AI Enthusiast
         </p>
-        <p className="text-base text-gray-400 tracking-wide">
-          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Facilis
-          ipsam autem cumque, accusantium dicta odio.
+        {/* <p className="text-base text-gray-400 tracking-wide">
+          Passionate about building responsive, user-friendly, and scalable web
+          applications. Experienced with React, Tailwind CSS, Firebase, and always
+          eager to learn new technologies.
+        </p> */}
+        <p className="text-base text-gray-400 flex items-center gap-2">
+          Phone: <span className="text-lightText">+233 55 413 9957</span>
         </p>
         <p className="text-base text-gray-400 flex items-center gap-2">
-          Phone: <span className="text-lightText">+968 97859628</span>
-        </p>
-        <p className="text-base text-gray-400 flex items-center gap-2">
-          Email: <span className="text-lightText">noor.jsdivs@gmail.com</span>
+          Email: <span className="text-lightText">owusuansaheric08@gmail.com</span>
         </p>
       </div>
       <div className="flex flex-col gap-4">
-        <h2 className="text-base uppercase font-titleFont mb-4">Find me in</h2>
+        <h2 className="text-base uppercase font-titleFont mb-4">Find me on</h2>
         <div className="flex gap-4">
-          <span className="bannerIcon">
+          {/* <a href="https://facebook.com/" target="_blank" rel="noreferrer" className="bannerIcon">
             <FaFacebookF />
-          </span>
-          <span className="bannerIcon">
-            <FaTwitter />
-          </span>
-          <span className="bannerIcon">
+          </a> */}
+          <a href="https://www.linkedin.com/in/eric-owusu-ansah-0365682b3/" target="_blank" rel="noreferrer" className="bannerIcon">
             <FaLinkedinIn />
-          </span>
+          </a>
+          <a href="https://github.com/alpha-eric08" target="_blank" rel="noreferrer" className="bannerIcon">
+            <FaGithub />
+          </a>
         </div>
       </div>
     </div>
   );
 }
 
-export default ContactLeft
+export default ContactLeft;
